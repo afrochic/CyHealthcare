67d56c87e702502f948b61ba77fd5c059c191510
afrochic <museediana@ymail.com>
2022-11-20 13:40:39 +0300

    intial commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const covidRoutes = require('./routes/covid.js')
 
